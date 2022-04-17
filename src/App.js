9360d7dcc6881f289b5fa20c5b4aa11fd3a7f460
fkadeal <infkadeal@gmail.com>
2022-04-17 00:11:13 -0800

    Working with useEffects it equivalent to watch in vue.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React ,{ useState} from 'react';
+import React ,{ useState, useEffect} from 'react';
 import './App.css';
 
 function App() { 
 const [_emotion, _setEmotion] = useState("happy");
+const [_secondary, _setSecondary] = useState("happy");
+useEffect( () => { console.log(`it's ${_emotion}!! around hire`); },[_emotion])
+useEffect( () => { console.log(`it's ${_secondary}!! around hire`); },[_secondary])
+
 
   return (
   <>
@@ -10,6 +14,9 @@ const [_emotion, _setEmotion] = useState("happy");
     <button onClick={() => _setEmotion("Happy")}>Happy</button>
     <button onClick={() => _setEmotion("Frustrate")}>Frustrate</button>
     <button onClick={() => _setEmotion("Enthusiastic")}>Enthuse</button>
+
+    <button onClick={() => _setSecondary("Secondary")}>Secondary</button>
+    <button onClick={() => _setSecondary("Mos")}>what is Mos</button>
   </>
   ); 
 }
