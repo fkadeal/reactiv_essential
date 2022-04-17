74e2bbfb4fb1057de88f4a5a30311a6babd492f3
fkadeal <infkadeal@gmail.com>
2022-04-17 01:14:16 -0800

    useReduser

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React ,{ useState, useEffect} from 'react';
+import React , { useState } from 'react';
 import './App.css';
-
+ 
 function App() { 
-const [_emotion, _setEmotion] = useState("happy");
-const [_secondary, _setSecondary] = useState("happy");
-useEffect( () => { console.log(`it's ${_emotion}!! around hire`); },[_emotion])
-useEffect( () => { console.log(`it's ${_secondary}!! around hire`); },[_secondary])
+const [checked, _setEmotion] = useState(false);
 
+function toggle(){
+  _setEmotion((checked) => !checked);
+}
 
   return (
-  <>
-  <h1>Click to change, your Emotion is Now = {_emotion}</h1>
-    <button onClick={() => _setEmotion("Happy")}>Happy</button>
-    <button onClick={() => _setEmotion("Frustrate")}>Frustrate</button>
-    <button onClick={() => _setEmotion("Enthusiastic")}>Enthuse</button>
-
-    <button onClick={() => _setSecondary("Secondary")}>Secondary</button>
-    <button onClick={() => _setSecondary("Mos")}>what is Mos</button>
+  <> 
+     <input type="checkbox" value={checked} onChange={toggle}></input>
+  <p>{checked ? "checked" : "notChecked"}</p>
   </>
   ); 
 }
