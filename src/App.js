935cb2827eb2dc719af190bfe59614895abc080a
fkadeal <infkadeal@gmail.com>
2022-04-20 17:59:54 -0800

    error handling

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import './App.css';
  
 function App({login}) { 
   const [data, setData] = useState(null);
+  const [loading , setLoading] = useState(false);
+  const [error , setError] = useState(null);
  
   useEffect( () => { 
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
     .then((response) => response.json())
-    .then(setData); 
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError)
   }, [login]);
 
-  if (data){
-    return <div>{JSON.stringify(data)}</div>
-  }  
-  return <div>no data</div>; 
+  if(loading) return <h1>Loading . . . </h1>; 
+  if(error) 
+  return <pre>{JSON.stringify(error)}</pre>
+  if(!data) return null; 
+
+    return (<div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url} />
+    </div>);  
 }
 
 export default App;
